edb7fabc80e8d8f1264f1059e04ace17b3e63eb5
WilliamDTorres <95170244+WilliamDTorres@users.noreply.github.com>
2021-12-15 18:06:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Okteto: Hello world!');
 })
 
-app.listen(3000, function () {
-  console.log('Starting hello-world server...');
+app.listen(port, function () {
+  console.log(`Starting hello-world server on port ${port}...`);
 })
