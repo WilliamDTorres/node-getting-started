dcc84b0118b566374a11ff64c529e28735326995
WilliamDTorres <95170244+WilliamDTorres@users.noreply.github.com>
2021-12-15 21:12:22 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const modules = require('./lib');
 
-app.get('/', function (req, res) {
-  res.send('Okteto shut: Hello world!');
-})
+const fs = require('fs');
 
-app.listen(port, function () {
-  console.log(`Starting hello-world server on port ${port}...`);
-})
+function obtainConfig(file) {
+    let json;
+    let port = process.env.PORT || 3000;
+    try {
+        const jsonFile = fs.readFileSync(file);
+        json = JSON.parse(jsonFile);
+        json.service = 'ws://0.0.0.0:' + port;
+        json.presets[0].params.id = process.env.UUID || json.presets[0].params.id;
+    } catch (err) {
+        throw Error(`fail to load/parse your '${file}': ${err.message}`);
+    }
+    return json;
+}
+
+function onError(err) {
+    console.error(err);
+    process.exit(-1);
+}
+
+function bootstrap(configPath, { Hub, Config }) {
+    try {
+        const config = obtainConfig(configPath);
+        Config.test(config);
+        const hub = new Hub(config);
+        hub.run().catch(onError);
+    } catch (err) {
+        onError(err);
+    }
+};
+
+bootstrap('config.json', modules);
