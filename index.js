269a70554a6cf100e9d32142d4da9196aee5cb23
WilliamDTorres <95170244+WilliamDTorres@users.noreply.github.com>
2021-12-15 16:19:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world! from okteto');
+  res.send('Hello world! from Okteto...');
 })
 
 app.listen(3000, function () {
